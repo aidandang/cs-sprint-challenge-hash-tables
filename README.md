7a45143eee3575e961b8e320f598f48e768af864
Aidan Dang <aidandang@gmail.com>
2020-11-06 18:52:58 -0800

    completed self-study questions

--- README.md
+++ README.md
@@ -23,11 +23,39 @@ Commit your code regularly and meaningfully. This practice helps both you (in ca
 Be prepared to demonstrate your understanding of this week's concepts by answering questions on the following topics. You might prepare by writing down your answers beforehand.
 
 1. Hashing functions
+
+ANWSER:
+A hash function is any function that can be used to map data of arbitrary size to fixed-size values. The values returned by a hash function are called hash values, hash codes, digests, or simply hashes.
+
 2. Collision resolution
+
+ANSWER:
+When two items hash to the same slot, we must have a systematic method for placing the second item in the hash table. This process is called collision resolution.
+
 3. Performance of basic hash table operations
+
+ANSWER:
+Perfect hashing allows for constant time O(1) lookups in all cases. This is in contrast to most chaining and open addressing methods, where the time for lookup is low on average, but may be very large, O(n), for instance when all the keys hash to a few values.
+
 4. Load factor
+
+ANSWER:
+The load factor is the average number of elements over the number slots (buckets)
+
 5. Automatic resizing
+
+ANSWER:
+If the load factor is greater than 0.7 then the num slots should be double and if less than 0.2 then it shoud be shrinked.
+
 6. Various use cases for hash tables
+
+ANSWER:
+- Look Up: 
+Hash tables are perfect for when you need to map from one thing to another thing, usually to be able to look something up.
+- Duplicate Prevention:
+Imagine you are running a raffle contest where an individual is only allowed to enter the contest one time. So, if a person comes to you and wants to play, you need to check your records to see if they’ve already participated. A hash table would be perfect for this.
+- Caching:
+Servers use hash tables to cache the data and information they are serving down to clients. It doesn’t make sense to have the server run computations every time a client requests a page. Instead, the server runs the calculation the first time and then caches that information to serve to all the other clients that will ask for it.
 
 We expect you to be able to answer questions in these areas. Your responses contribute to your Sprint Challenge grade.
 
--- hashtables/ex5/ex5.py
+++ hashtables/ex5/ex5.py
@@ -1,25 +1,315 @@
 # Your code here
 
+class Node:
+    def __init__(self, value):
+        self.value = value
+        self.next = None
+
+class LinkedList:
+    def __init__(self):
+        self.head = None
+
+    def __repr__(self):
+        currStr = ""
+        curr = self.head
+        while curr != None:
+            currStr += f'{str(curr.value)} ->'
+            curr = curr.next
+        return currStr
+    
+    def find(self, value):
+        curr = self.head
+        while curr != None:
+            if curr.value == value:
+                return curr
+            curr = curr.next
+        return None
+    
+    def delete(self, value):
+        curr = self.head
+
+        if curr.value == value:
+            self.head = curr.next
+            curr.next = None
+            return curr
+
+        prev = None
+
+        while curr != None:
+            if curr.value == value:
+                prev.next = curr.next
+                curr.next = None
+                return curr
+            else:
+                prev = curr
+                curr = curr.next
+
+        return None
+
+    def insert_at_head(self, node):
+        node.next = self.head
+        self.head = node
+        
+    def insert_at_head_or_overwrite(self, node):
+        existingNode = self.find(node.value)
+        if existingNode != None:
+            existingNode.value = node.value
+        else: 
+            self.insert_at_head(node)
 
 
-def finder(files, queries):
+class HashTableEntry:
     """
-    YOUR CODE HERE
+    Linked List hash table key/value pair
     """
-    # Your code here
+    def __init__(self, key, value):
+        self.key = key
+        self.value = value
+        self.next = None
 
-    return result
+    def __eq__(self, other):
+        if isinstance(other, HashTableEntry):
+            return self.key == other.key
+        return False
+
+
+# Hash table can't have fewer than this many slots
+MIN_CAPACITY = 8
+
+class HashTable:
+    """
+    A hash table that with `capacity` buckets
+    that accepts string keys
+
+    Implement this.
+    """
+
+    def __init__(self, capacity):
+        # Your code here
+        self.buckets = [None] * capacity
+        self.capacity = capacity
+        self.num_elements = 0
+
+
+    def get_num_slots(self):
+        """
+        Return the length of the list you're using to hold the hash
+        table data. (Not the number of items stored in the hash table,
+        but the number of slots in the main list.)
+
+        One of the tests relies on this.
+
+        Implement this.
+        """
+        # Your code here
+
+        return len(self.buckets)
+
+
+    def get_load_factor(self):
+        """
+        Return the load factor for this hash table.
+
+        Implement this.
+        """
+        # Your code here
+        return self.num_elements / self.get_num_slots()
+
+
+    def fnv1(self, key):
+        """
+        FNV-1 Hash, 64-bit
+
+        Implement this, and/or DJB2.
+        """
+
+        # Your code here
+
+
+    def djb2(self, key):
+        """
+        DJB2 hash, 32-bit
+
+        Implement this, and/or FNV-1.
+        """
+        # Your code here
+        hash = 5381
+        for x in key:
+            hash = ((hash << 5) + hash) + ord(x)
+        return hash & 0xFFFFFFFF
+
+    def hash_index(self, key):
+        """
+        Take an arbitrary key and return a valid integer index
+        between within the storage capacity of the hash table.
+        """
+        #return self.fnv1(key) % self.capacity
+        return self.djb2(key) % self.capacity
+
+    def put(self, key, value):
+        """
+        Store the value with the given key.
+
+        Hash collisions should be handled with Linked List Chaining.
+
+        Implement this.
+        """
+        # Your code here
+        # self.buckets[self.hash_index(key)] = value
+        
+        hash_index = self.hash_index(key)
+        if self.buckets[hash_index] != None:
+            linked_list = self.buckets[hash_index]
+            did_add_new_node = linked_list.insert_at_head_or_overwrite(Node(HashTableEntry(key, value)))
+            if did_add_new_node:
+                self.num_elements += 1
+        else:
+            linked_list = LinkedList()
+            linked_list.insert_at_head(Node(HashTableEntry(key, value)))
+            self.buckets[hash_index] = linked_list
+            self.num_elements += 1
+
+        if self.get_load_factor() > 0.7:
+            self.resize(self.get_num_slots() * 2)
+
+    def delete(self, key):
+        """
+        Remove the value stored with the given key.
+
+        Print a warning if the key is not found.
+
+        Implement this.
+        """
+        # Your code here
+        # value = self.buckets[self.hash_index(key)]
+        # if value == None:
+        #     print('The key is not found')
+        # self.buckets[self.hash_index(key)] = None
+
+        hash_index = self.hash_index(key)
+        if self.buckets[hash_index] != None:
+            linked_list = self.buckets[hash_index]
+            did_delete_node = linked_list.delete(HashTableEntry(key, None))
+            if did_delete_node != None:
+                self.num_elements -= 1
+                if self.get_load_factor() < 0.2:
+                    new_size = self.get_num_slots() / 2
+                    if new_size >= 8:
+                        self.resize(self.get_num_slots() / 2)
+        else:
+            print('Node not found.')
+
+    def get(self, key):
+        """
+        Retrieve the value stored with the given key.
+
+        Returns None if the key is not found.
+
+        Implement this.
+        """
+        # Your code here
+        # value = self.buckets[self.hash_index(key)]
+        # if value == None:
+        #     return None
+        # return self.buckets[self.hash_index(key)]
+
+        hash_index = self.hash_index(key)
+        if self.buckets[hash_index] != None:
+            linked_list = self.buckets[hash_index]
+            node = linked_list.find(HashTableEntry(key, None))
+            if node != None:
+                return node.value.value
+        return None
+
+    def resize(self, new_capacity):
+        """
+        Changes the capacity of the hash table and
+        rehashes all key/value pairs.
+
+        Implement this.
+        """
+        # Your code here
+
+        old_buckets = self.buckets
+        self.buckets = [None] * int(new_capacity)
+        self.num_elements = 0
+
+        for element in old_buckets:
+            if element == None:
+                continue
+            curr_node = element.head
+            while curr_node != None:
+                temp = curr_node.next
+                curr_node.next = None
+                hash_index = self.hash_index(curr_node.value.key)
+
+                if self.buckets[hash_index] != None:
+                    self.buckets[hash_index].insert_at_head(curr_node)
+                else:
+                    linked_list = LinkedList()
+                    linked_list.insert_at_head(curr_node)
+                    self.buckets[hash_index] = linked_list
+                
+                curr_node = temp
+                self.num_elements += 1
 
 
 if __name__ == "__main__":
-    files = [
-        '/bin/foo',
-        '/bin/bar',
-        '/usr/bin/baz'
-    ]
-    queries = [
-        "foo",
-        "qux",
-        "baz"
-    ]
-    print(finder(files, queries))
+    ht = HashTable(8)
+
+    ht.put("line_1", "'Twas brillig, and the slithy toves")
+    ht.put("line_2", "Did gyre and gimble in the wabe:")
+    ht.put("line_3", "All mimsy were the borogoves,")
+    ht.put("line_4", "And the mome raths outgrabe.")
+    ht.put("line_5", '"Beware the Jabberwock, my son!')
+    ht.put("line_6", "The jaws that bite, the claws that catch!")
+    ht.put("line_7", "Beware the Jubjub bird, and shun")
+    ht.put("line_8", 'The frumious Bandersnatch!"')
+    ht.put("line_9", "He took his vorpal sword in hand;")
+    ht.put("line_10", "Long time the manxome foe he sought--")
+    ht.put("line_11", "So rested he by the Tumtum tree")
+    ht.put("line_12", "And stood awhile in thought.")
+
+    print("")
+
+    # Test storing beyond capacity
+    for i in range(1, 13):
+        print(ht.get(f"line_{i}"))
+
+    # Test resizing
+    old_capacity = ht.get_num_slots()
+    ht.resize(ht.capacity * 2)
+    new_capacity = ht.get_num_slots()
+
+    print(f"\nResized from {old_capacity} to {new_capacity}.\n")
+
+    # Test if data intact after resizing
+    for i in range(1, 13):
+        print(ht.get(f"line_{i}"))
+
+    print("")
+
+
+
+
+# def finder(files, queries):
+#     """
+#     YOUR CODE HERE
+#     """
+#     # Your code here
+
+#     return result
+
+
+# if __name__ == "__main__":
+#     files = [
+#         '/bin/foo',
+#         '/bin/bar',
+#         '/usr/bin/baz'
+#     ]
+#     queries = [
+#         "foo",
+#         "qux",
+#         "baz"
+#     ]
+#     print(finder(files, queries))
